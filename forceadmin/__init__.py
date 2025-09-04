34010c372da9b718636d42ec37d88632b7caaef6
DidiDidi129 <mlambis2010@icloud.com>
2025-09-04 13:43:31 +0930

    okie

--- forceadmin/__init__.py
+++
@@ -1,4 +0,0 @@
-from .adminrole import AdminRole
-
-async def setup(bot):
-    await bot.add_cog(AdminRole(bot))
--- nekos/__init__.py
+++
@@ -1,4 +0,0 @@
-from .neko import Neko
-
-async def setup(bot):
-    await bot.add_cog(Neko(bot))
